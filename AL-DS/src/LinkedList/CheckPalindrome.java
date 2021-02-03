3c500766e8a9a4845e99fe9618669403786d4fec
Long Chen <chenlong890729@gmail.com>
2021-02-02 21:00:22 -0700

    First Commit to add AL-DS project

---
+++ AL-DS/src/LinkedList/CheckPalindrome.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+public class CheckPalindrome {
+	public static void main(String[] args) {
+		//1-2-1-1
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(1);
+		head.next.next.next = new ListNode(1);
+		
+//		CheckPalindrome.print(head);		
+		boolean res = CheckPalindrome.isPalindrome(head);
+		System.out.println(res);
+	}
+	private static boolean isPalindrome(ListNode head) {
+		if(head == null || head.next == null) {
+			return true;
+		}
+		ListNode curr = head;
+//		CheckPalindrome.print(curr);
+		
+		ListNode reverseHead = reverse(head);
+//		CheckPalindrome.print(reverseHead);
+		while(curr != null ) {
+			if(curr.val != reverseHead.val) {
+				return false;
+			}
+			curr = curr.next;
+			reverseHead = reverseHead.next;
+		}
+		return true;
+	}
+	private static ListNode reverse(ListNode head) {
+		if(head == null || head.next == null) {
+			return head;
+		}
+		ListNode newHead = reverse(head.next);
+		head.next.next =head;
+		head.next = null;
+		return newHead;
+	}
+	private static void print(ListNode head) {
+		while(head!=null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+
+}
