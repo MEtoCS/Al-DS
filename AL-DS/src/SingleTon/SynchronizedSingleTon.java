1ddc7b5d2a77b12ff55d36d7649c08a70379dc01
Long Chen <chenlong890729@gmail.com>
2021-02-15 02:46:58 -0700

    create Synchronized SingleTon

---
+++ AL-DS/src/SingleTon/SynchronizedSingleTon.java
@@ -0,0 +1,39 @@
+package SingleTon;
+
+public class SynchronizedSingleTon  implements Cloneable{
+	private static volatile SynchronizedSingleTon instance ;
+	
+	private SynchronizedSingleTon() {
+		//to avoid reflection, constructor works only first time
+		if(instance !=null) {
+			throw new RuntimeException("Object already created");
+		}
+	}
+	//to avoid deserialization
+	protected Object readResolve() {
+		return instance;
+	}
+	
+	//to avoid clone
+	public Object clone() throws CloneNotSupportedException{
+		throw new CloneNotSupportedException();
+	}
+	
+	public static SynchronizedSingleTon getInstance() {
+		//first null check, improve the performance, 
+		//otherwise, it will lock all the thread to 
+		//create instance, even there is already one instance
+		if(instance==null) {  
+			synchronized(SynchronizedSingleTon.class){
+				//it is possible to create multiple instance
+				//thread1 is null, create one, thread2 may create other one
+				if(instance==null) { 
+					instance = new SynchronizedSingleTon();
+				}
+			}
+			
+		}
+		return instance;
+	}
+
+}
